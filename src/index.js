9b3d8f5aa8f58097dfa36aa039dc4cfce2aa4fb7
eyeej <eyee.joy@gmail.com>
2025-07-10 13:35:41 +1000

    js changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 function refreshWeather(response) {
-    let TemperatureElement = document.querySelector("#temperature");
-    TemperatureElement.innerHTML = response.data.temperature.current;
-}
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    temperatureElement.innerHTML = response.data.temperature.current;
 
+    cityElement.innerHTML = response.data.city;
+    temperatureElement = Math.round(temperature);
+}
 function searchCity(city) {
     let apiKey = "t9e9139a19b801fbcfa020d17a47ob1f";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -12,8 +16,8 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
     event.preventDefault();
    let searchInput = document.querySelector("#search-form-input");
-   let cityElement = document.querySelector("#city");
-   cityElement.innerHTML = searchInput.value;
+   
+   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
