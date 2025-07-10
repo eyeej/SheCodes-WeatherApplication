bd788271ef6619fedfb20816b1c2a2874f3366d0
eyeej <eyee.joy@gmail.com>
2025-07-10 13:19:19 +1000

    api included

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function refreshWeather(response){
+    console.log(response.data);
+
+}
+
+function searchCity(city) {
+    let apiKey = "t9e9139a19b801fbcfa020d17a470b1f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
    let searchInput = document.querySelector("#search-form-input");
    let cityElement = document.querySelector("#city");
    cityElement.innerHTML = searchInput.value;
+   cityElement.innerHTML = city;
+   searchCity(city);
 }
 
 let searchFormElement = document.querySelector("#search-form");
